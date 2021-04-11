811a3f51dc914dc7039c0819d6dcc953e3754019
Connie Hyman <connie2630a@gmail.com>
2021-04-11 09:41:59 -0700

    Set colorArray to populate grid with random colors from blocks.png image

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -5,24 +5,32 @@ class SceneMain extends Phaser.Scene {
   preload() {
     //load our images or sounds
     this.load.spritesheet("blocks", "images/blocks.png", {
-      frameWidth: 63,
+      frameWidth: 64,
       frameHeight: 84,
     });
   }
   create() {
     //define our objects
+    this.colorArray = [];
+    for (let i = 0; i < 25; i++) {
+      let color = Phaser.Math.Between(0, 7);
+      this.colorArray.push(color);
+    }
     let xx = 0;
     let yy = 0;
     let block;
+    let count = 0;
     for (let i = 0; i < 5; i++) {
       for (let j = 0; j < 5; j++) {
         block = this.add.image(0, 0, "blocks");
         block.displayWidth = game.config.width / 5;
         block.displayHeight = game.config.height / 5;
+        block.setFrame(this.colorArray[count]);
         block.setOrigin(0, 0);
         block.x = xx;
         block.y = yy;
         xx += block.displayWidth;
+        count++;
       }
       xx = 0;
       yy += block.displayHeight;
