8f1b4705696374b44076eae319560742bbb50cce
Connie Hyman <connie2630a@gmail.com>
2021-04-11 09:21:49 -0700

    Set up block grid

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -11,6 +11,22 @@ class SceneMain extends Phaser.Scene {
   }
   create() {
     //define our objects
+    let xx = 0;
+    let yy = 0;
+    let block;
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 5; j++) {
+        block = this.add.image(0, 0, "blocks");
+        block.displayWidth = game.config.width / 5;
+        block.displayHeight = game.config.height / 5;
+        block.setOrigin(0, 0);
+        block.x = xx;
+        block.y = yy;
+        xx += block.displayWidth;
+      }
+      xx = 0;
+      yy += block.displayHeight;
+    }
   }
   update() {
     //constant running loop
