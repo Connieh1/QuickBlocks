b6a8d7aa41a08c5cbaf857b03627307881c94e94
Connie Hyman <connie2630a@gmail.com>
2021-04-11 10:40:00 -0700

    Set block positioning for better fall effect, build out pickColor function

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -27,11 +27,13 @@ class SceneMain extends Phaser.Scene {
         block.displayWidth = game.config.width / 5;
         block.displayHeight = game.config.height / 5;
         block.setFrame(this.colorArray[count]);
-        block.setOrigin(0, 0);
-        block.x = xx;
-        block.y = yy;
+        // block.setOrigin(0, 0);
+        block.x = xx + block.displayWidth / 2;
+        block.y = yy + block.displayHeight / 2;
         if (i == 2 && j == 2) {
           this.centerBlock = block;
+        } else {
+          block.setInteractive();
         }
         xx += block.displayWidth;
         count++;
@@ -42,10 +44,33 @@ class SceneMain extends Phaser.Scene {
     this.colorArray[12] = -1;
 
     this.pickColor();
+
+    this.input.on("gameobjectdown", this.selectBlock, this);
+  }
+
+  selectBlock(pointer, block) {
+    if (block.frame.name == this.centerBlock.frame.name) {
+      block.removeInteractive();
+      this.fall(block);
+      this;
+    }
+  }
+
+  fall(block) {
+    this.tweens.add({ targets: block, duration: 1000, scaleX: 0, scaleY: 0 });
   }
 
   pickColor() {
-    let color = this.colorArray.shift();
+    let color;
+    if (this.colorArray.length == 0) {
+      console.log("Next Level");
+      return;
+    }
+    color = this.colorArray.shift();
+    if (color == -1) {
+      this.pickColor();
+      return;
+    }
     this.centerBlock.setFrame(color);
   }
 
